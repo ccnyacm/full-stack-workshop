934bf57dafefdbf3c08be3200545e2e1ee132692
Hugo Meza <hgmeza114@gmail.com>
2019-03-28 19:53:27 -0400

    day 1 work

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,25 @@
-console.log('You are ready for the workshop');
+require('dotenv').config();
+require('./db/mongoose');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+const auth = require('./routes/authentication');
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+// handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => { console.log('connection to database is successful\n')});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/authentication/signup', auth.signup);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
